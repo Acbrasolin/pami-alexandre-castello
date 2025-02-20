8c88509d757e038a102fc7b708790d1045d2c218
Alexandre Castello <juniorbrasolin3@gmail.com>
2025-02-20 08:29:00 -0300

    aula sobre os tipos de variáveis + especificações

---
+++ aula02/variaveis.js
@@ -0,0 +1,37 @@
+var numero = 2;
+let texto = "Hello";
+const oQueEisso = "?";
+
+
+//o ponto e virgula é opcional! Boas praticas os usam
+
+
+
+
+console.log("Tipo var: ", numero);
+
+//concatenar com virgula me garante um espaço
+
+numero = 13;
+
+console.log(texto + " o numero agora é " + numero);
+console.log(`${texto} o numero agora é ${numero}`)
+texto = 69;
+
+console.log(texto+numero);
+//concatenar com + dois valores numericos vai
+//acabar somando
+
+console.log(oQueEisso)
+texto = '!'
+console.log(texto)
+//let pode ser transcrevido, constantes não.
+
+//constantes não podem ter novos valores assinalados a ela
+
+//oQueEisso = "um constante????"
+
+//console.log(oQueEisso)
+
+//https://www.alura.com.br/artigos/convencoes-nomenclatura-camel-pascal-kebab-snake-case?srsltid=AfmBOopK_NOaKw19dGrg93Wq4_7pZnvCldnTse5tprIjRqE7OmhfRfow
+//link acima dos tipos de escrever variaveis pela alura.
