38ea6db0c89385663767db38c7cb88c62f3a9370
Alexandre <juniorbrasolin3@gmail.com>
2025-08-22 01:16:21 -0300

    atualizações do projeto expo

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,14 +1,51 @@
 import { Text, View, StyleSheet } from 'react-native';
  import { Link } from 'expo-router'; 
+ import { Image } from 'expo-image';
+ import ImageViewer from '@/components/ImageViewer';
+ import Button from '@/components/Button';
+ import * as ImagePicker from 'expo-image-picker';
+ import { useState } from 'react';
+
+
+
+const PlaceholderImage = require('@/assets/images/cachorro.jpg');
+require('@/assets/images/cachorro2.jpg');
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+       <View style={styles.imageContainer}>
+         <ImageViewer imgSource={selectedImage || PlaceholderImage}/>
+       
+        
+      </View>
+       <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" />
+        
+      </View>
+      <Text style={styles.text}></Text>
       <Link href="/about" style={styles.button}>
-        Go to About screen
+        
       </Link>
+      
     </View>
+    
   );
 }
 
@@ -26,6 +63,18 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff',
+  },
+   imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
 
